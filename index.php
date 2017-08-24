ce5a261dfe40b84b7799c81062eb0c043fdba98f
webdatavisualdev <dai.yamamoto324@hotmail.com>
2017-08-24 16:09:13 +0300

    Change mysql access

--- index.php
+++ index.php
@@ -1,97 +1,66 @@
 <?php
-// // shell_exec("ssh -f -L 3307:127.0.0.1:8082 master_qhefhbeemq@45.76.240.68 sleep 60 >> logfile");  
-// $db = mysqli_connect('localhost', "wmaavxkuss", "2tsD2fA42e", "wmaavxkuss", 3307);
-// mysql_select_db( "db", $db );
-// if (!$db) {
-//     echo "Error: Unable to connect to MySQL." . PHP_EOL;
-//     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-//     echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-//     exit;
-// }
-$mysqli = new mysqli("localhost", "wmaavxkuss", "2tsD2fA42e", "wmaavxkuss");
-if ($mysqli->ping()) {
-    printf ("Our connection is ok!\n");
-} else {
-    printf ("Error: %s\n", $mysqli->error);
-}
-
-	// $servername = "45.76.240.68:8082";
-	// $username = "wmaavxkuss";
-	// $password = "2tsD2fA42e";
+	$db = new db();
 	
-	// try {
-	// 	$conn = new PDO("mysql:host=$servername;dbname=wmaavxkuss", $username, $password);
-	// 	// set the PDO error mode to exception
-	// 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	// 	echo "Connected successfully"; 
-	// 	}
-	// catch(PDOException $e)
-	// 	{
-	// 	echo "Connection failed: " . $e->getMessage();
-	// 	die("Error");
-	// 	}
-	// $db = new db();
+	$results = $db->query("SHOW VARIABLES LIKE '%timeout%'", TRUE);
+	echo "<pre>";
+	var_dump($results);
+	echo "</pre>";
 	
-	// $results = $db->query("SHOW VARIABLES LIKE '%timeout%'", TRUE);
-	// echo "<pre>";
-	// var_dump($results);
-	// echo "</pre>";
+	$results = $db->query("SET session wait_timeout=28800", FALSE);
+	// UPDATE - this is also needed
+	$results = $db->query("SET session interactive_timeout=28800", FALSE);
 	
-	// $results = $db->query("SET session wait_timeout=28800", FALSE);
-	// // UPDATE - this is also needed
-	// $results = $db->query("SET session interactive_timeout=28800", FALSE);
-	
-	// $results = $db->query("SHOW VARIABLES LIKE '%timeout%'", TRUE);
-	// echo "<pre>";
-	// var_dump($results);
-	// echo "</pre>";
+	$results = $db->query("SHOW VARIABLES LIKE '%timeout%'", TRUE);
+	echo "<pre>";
+	var_dump($results);
+	echo "</pre>";
 	
 	
-	// class db {
+	class db {
 	
-	// 	public $mysqli;
+		public $mysqli;
 	
-	// 	public function __construct() {
-	// 		$this->mysqli = new mysqli("45.76.20.111:8082", "kmpscazrwg", "5VkSQGX5Gc", "intrinio_close_price");
-	// 		if (mysqli_connect_errno()) {
-	// 			exit();
-	// 		}
-	// 	}
+		public function __construct() {
+			$this->mysqli = new mysqli("localhost", "kmpscazrwg", "5VkSQGX5Gc", "kmpscazrwg");
+			if (mysqli_connect_errno()) {
+				exit();
+			}
+		}
 	
-	// 	public function __destruct() {
-	// 		$this->disconnect();
-	// 		unset($this->mysqli);
-	// 	}
+		public function __destruct() {
+			$this->disconnect();
+			unset($this->mysqli);
+		}
 	
-	// 	public function disconnect() {
-	// 		$this->mysqli->close();
-	// 	}
+		public function disconnect() {
+			$this->mysqli->close();
+		}
 	
-	// 	function query($q, $resultset) {
+		function query($q, $resultset) {
 	
-	// 		/* create a prepared statement */
-	// 		if (!($stmt = $this->mysqli->prepare($q))) {
-	// 			echo("Sql Error: " . $q . ' Sql error #: ' . $this->mysqli->errno . ' - ' . $this->mysqli->error);
-	// 			return false;
-	// 		}
+			/* create a prepared statement */
+			if (!($stmt = $this->mysqli->prepare($q))) {
+				echo("Sql Error: " . $q . ' Sql error #: ' . $this->mysqli->errno . ' - ' . $this->mysqli->error);
+				return false;
+			}
 	
-	// 		/* execute query */
-	// 		$stmt->execute();
+			/* execute query */
+			$stmt->execute();
 	
-	// 		if ($stmt->errno) {
-	// 			echo("Sql Error: " . $q . ' Sql error #: ' . $stmt->errno . ' - ' . $stmt->error);
-	// 			return false;
-	// 		}
-	// 		if ($resultset) {
-	// 			$result = $stmt->get_result();
-	// 			for ($set = array(); $row = $result->fetch_assoc();) {
-	// 			$set[] = $row;
-	// 			}
-	// 			$stmt->close();
-	// 			return $set;
-	// 		}
-	// 	}
-	// }
+			if ($stmt->errno) {
+				echo("Sql Error: " . $q . ' Sql error #: ' . $stmt->errno . ' - ' . $stmt->error);
+				return false;
+			}
+			if ($resultset) {
+				$result = $stmt->get_result();
+				for ($set = array(); $row = $result->fetch_assoc();) {
+				$set[] = $row;
+				}
+				$stmt->close();
+				return $set;
+			}
+		}
+	}
 ?>
 <!DOCTYPE html>
 <head>
