0ec1e1b47c5ac1549b1f3bd8c4a049e3c9c00d18
webdatavisualdev <dai.yamamoto324@hotmail.com>
2017-08-24 20:09:34 +0300

    Make data.php

--- index.php
+++ index.php
@@ -88,7 +88,7 @@
 	</div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script>
-	var dateGroupIndex, newsData, chart, totalData;
+	var dateGroupIndex, newsData, chart, totalData = [];
 	var maxDate = 0;
 	var displayCount = 500;
 	var groupingArryIndex = [];
