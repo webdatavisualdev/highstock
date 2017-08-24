5e9f08b364c1cbfd91dda0eb600ccd6f4f925ca4
webdatavisualdev <dai.yamamoto324@hotmail.com>
2017-08-24 19:44:10 +0300

    Change mysql access

--- index.php
+++ index.php
@@ -142,6 +142,7 @@
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
+		console.log(this.responseText);
         var myObj = JSON.parse(this.responseText);
         console.log(myObj.name);
     }
