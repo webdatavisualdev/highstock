7f448c0390944b39c6c6326a77e44675ddfd9bb5
webdatavisualdev <dai.yamamoto324@hotmail.com>
2017-08-25 14:58:29 +0300

    Make data.php

--- chartdata.php
+++ chartdata.php
@@ -3,7 +3,7 @@
 	$date = date("y-m-d");
 	echo $date;
 	$db = new db();
-	$results = $db->query("SELECT * FROM intrinio_close_price WHERE date between '2012-1-1' AND '".$date."' LIMIT 15000", TRUE);
+	$results = $db->query("SELECT * FROM stock_volumn WHERE volume > 0 AND do_date between '2012-1-1' AND '".$date."'", TRUE);
 
 	echo json_encode($results);
 
