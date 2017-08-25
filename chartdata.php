b158f11be7295d5264f014f692045db753ae95b0
webdatavisualdev <dai.yamamoto324@hotmail.com>
2017-08-25 09:42:49 +0300

    Make data.php

--- chartdata.php
+++ chartdata.php
@@ -1,7 +1,7 @@
 <?php
 
 	$db = new db();
-	$results = $db->query("SELECT * FROM EOD_stock_price_history WHERE type = 'stockprice' AND s_timestamp BETWEEN '1/1/2012' AND '".date("m/d/y")."'", TRUE);
+	$results = $db->query("SELECT * FROM EOD_stock_price_history WHERE type = 'stockprice' AND s_timestamp between '1/1/2012' AND '8/30/2017'", TRUE);
 
 	echo json_encode($results);
 
